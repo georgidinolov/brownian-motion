3cc04d26eefe14fe8e98b062ff09c80daa82d71a
georgidinolov <gdinolov@soe.ucsc.edu>
2017-02-12 16:25:26 -0800

    for simulating on the server

--- 2DBrownianMotionPath.hpp
+++ 2DBrownianMotionPath.hpp
@@ -1,3 +1,4 @@
+#include <armadillo>
 #include <vector>
 
 class BrownianMotion
--- 2d-brownian-motion-data-sets.cpp
+++ 2d-brownian-motion-data-sets.cpp
@@ -8,17 +8,17 @@ using namespace std;
 
 int main() 
 {
-  unsigned order = 10000;
+  unsigned order = 1000;
   double sigma_x = 1.0;
   double sigma_y = 1.0;
   double rho = 0.8;
   double x_0 = 0;
   double y_0 = 0;
   double t = 1;
-  unsigned number_samples = 50;
-  unsigned number_observations = 10;
+  unsigned number_data_sets = 100;
+  unsigned number_observations_per_data_set = 128;
 
-  for (unsigned i=0; i<number_samples; ++i) {
+  for (unsigned i=0; i<number_data_sets; ++i) {
     std::ofstream path_file;
     std::string file_name = "data-set-" +
       std::to_string(i+1) +
@@ -28,11 +28,12 @@ int main()
       // header
       path_file << "sigma_x, sigma_y, rho, x_0, y_0, t, a, x_T, b, c, y_T, d \n";
       
-      for (unsigned j=0; j<number_observations; ++j) {
+      for (unsigned j=0; j<number_observations_per_data_set; ++j) {
 
-  	unsigned seed = i*number_observations + j;
+  	unsigned seed = i*number_observations_per_data_set + j;
+
   	BrownianMotion BM = BrownianMotion(seed,
-  					   order,
+					   order,
   					   rho,
   					   sigma_x,
   					   sigma_y,
